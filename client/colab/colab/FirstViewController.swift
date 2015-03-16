202e1f300f441c82ee3fd4aa37d1ba321e727251
Andrew Gold <andrewmichaelgold@gmail.com>
2015-03-16 13:16:01 -0500

    initial commit

---
+++ client/colab/colab/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  colab
+//
+//  Created by Andrew Gold on 3/16/15.
+//  Copyright (c) 2015 goldreddyfishkin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
